4ecdcdd9af4226730093d26d63d5c59e9a788db5
Leo P <junk@slact.net>
2015-12-02 00:51:43 -0500

    github flavored markdown templatier and other thing. whatever.

---
+++ config/initializers/gfm.rb
@@ -0,0 +1,28 @@
+require "tilt"
+require "redcarpet"
+require "pygments"
+
+class HTMLwithPygments < Redcarpet::Render::HTML
+  def block_code(code, language)
+    Pygments.highlight(code, lexer: language)
+  end
+end
+
+module Tilt                                                                                                                                                
+  class GithubFlavoredMarkdownTemplate < Template
+    def prepare
+      @engine = Redcarpet::Markdown.new(HTMLwithPygments, fenced_code_blocks: true, autolink: true, strikethrough: true, no_intra_emphasis: true, disable_indented_code_blocks: true)
+    end
+
+    def evaluate(scope, locals, &block)
+      @output ||= @engine.render(data)
+    end
+
+    def allows_script?
+      false
+    end
+  end
+end
+
+
+Tilt.register Tilt::GithubFlavoredMarkdownTemplate, 'md'
