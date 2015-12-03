54e327afa8291a3c7e103f3f3700b13ff4b8c3dc
Leo P <junk@slact.net>
2015-12-02 21:58:16 -0500

    lax spacing maybe

--- config/initializers/gfm.rb
+++ config/initializers/gfm.rb
@@ -6,15 +6,13 @@ class HTMLwithPygments < Redcarpet::Render::HTML
   def block_code(code, language)
     Pygments.highlight(code, lexer: language)
   end
-  
-  
 end
 
 module Tilt                                                                                                                                                
   class GithubFlavoredMarkdownTemplate < Template
     def prepare
       renderer = HTMLwithPygments.new(:with_toc_data => true)
-      @engine = Redcarpet::Markdown.new(renderer, fenced_code_blocks: true, autolink: true, strikethrough: true, no_intra_emphasis: true, disable_indented_code_blocks: true)
+      @engine = Redcarpet::Markdown.new(renderer, lax_spacing: true, fenced_code_blocks: true, autolink: true, strikethrough: true, no_intra_emphasis: true, disable_indented_code_blocks: true)
     end
 
     def evaluate(scope, locals, &block)
