d96de567d206759ee6530bafa77a47c9250ec600
Leo P <junk@slact.net>
2015-12-03 14:04:14 -0500

    serve packages

--- app/controllers/download.rb
+++ app/controllers/download.rb
@@ -1,8 +1,8 @@
 class Nchapp::DownloadController < Nchapp::ApplicationController
-  get '/nginx-nchan-latest.deb' do
+  def serve_pkg(glob)
     found = nil
     Dir.chdir "gitdir/nchan/dev/package/pkgs/" do 
-      found = Dir.glob("*.deb").first
+      found = Dir.glob(glob).first
     end
     if found
       response.redirect "/pkgs/#{found}"
@@ -10,6 +10,14 @@ class Nchapp::DownloadController < Nchapp::ApplicationController
       response.status = 404
       render '404'
     end
+  end
+  
+  get '/nginx-nchan-latest.deb' do
+    serve_pkg "*.deb"
+  end
+  
+  get '/nginx-nchan-latest.tar.gz' do
+    serve_pkg "*.tar.gz"
   end
   
   #404
