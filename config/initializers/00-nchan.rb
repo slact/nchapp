0a0b7febd5fb5f06353ab693f375c10ef0bff852
Leo P <junk@slact.net>
2015-12-03 20:21:58 -0500

    pygments.css static

--- config/initializers/00-nchan.rb
+++ config/initializers/00-nchan.rb
@@ -25,4 +25,4 @@ root_dir = Dir.pwd
 system "gitdir/nchan/dev/redocument.rb gitdir/nchan/ #{root_dir}/app/views/README.md"
 
 #generate pygments css
-File.write("app/assets/css/pygments.css", Pygments.css)
+#File.write("app/assets/css/pygments.css", Pygments.css)
