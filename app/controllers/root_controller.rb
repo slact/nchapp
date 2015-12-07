2ff0df1b97570edcc68937640b8425b5af613a0f
Leo P <junk@slact.net>
2015-12-07 14:07:44 -0500

    don't autoreload stuff, it doesn't really work

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -4,7 +4,7 @@ class Nchapp::RootController < Nchapp::ApplicationController
   end
   
   get '/readme' do
-    maybe_reload_templates 
+    #maybe_reload_templates 
     render 'README'
   end
   
@@ -13,7 +13,7 @@ class Nchapp::RootController < Nchapp::ApplicationController
   end
   
   get '/changelog' do
-    maybe_reload_templates 
+    #maybe_reload_templates 
     render 'changelog'
   end
   
