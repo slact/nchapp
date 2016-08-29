e823019c43ed881c6f8b4f2b2a39072444d68f75
Leo P <junk@slact.net>
2016-08-29 18:34:35 -0400

    added contents tables

--- app/assets/js/common.js
+++ app/assets/js/common.js
@@ -1,20 +1,76 @@
+"use strict";
 
-var subUrl="/pubsub/nchan";
-
-function newSub(subType) {
-  window.nchanSub=new MessageMonitor(subUrl, subType);
-  window.nchanSub.addEvent('message', function(msg, id) {
-    console.log(msg, id);
+function insertTableOfContents(tocElSelector) {
+  var tocContainer = document.getElement(tocElSelector);
+  if(!tocContainer) {
+    return;
+  }
+  
+  var els = tocContainer.getParent().getElements(tocElSelector + ", h1, h2, h3, h4, h5");
+  var tocSeen = false;
+  
+  var hs=[];
+  var current_heading;
+  
+  var toc= new Element('ul', {'class': 'tableOfContents'});
+  var lastUl = toc;
+  var lastLevel;
+  var lastLi;
+  var headingsFound = 0;
+  
+  var makeLi = function(el) {
+    var li = new Element('li');
+    li.adopt(new Element('a', {'text': el.get('text'), 'href':"#" + el.get('id')}));
+    return li;
+  };
+  
+  Array.each(els, function(el, i) {
+    if(el == tocContainer) {
+      tocSeen = true;
+    }
+    else if(tocSeen) {
+      headingsFound ++;
+      var lvl = parseInt(el.get('tag')[1]);
+      if(!lastLevel) {
+        lastLevel = lvl;
+      }
+      
+      if(lastLevel == lvl) {
+        lastLi = makeLi(el);
+        lastUl.adopt(lastLi);
+      }
+      else if(lvl > lastLevel) {
+        lastUl = new Element('ul');
+        lastLi.adopt(lastUl);
+        lastLi = makeLi(el);
+        lastUl.adopt(lastLi);
+        lastLevel = lvl;
+      }
+      else {
+        if(lastUl == toc) {
+          throw "oh no!";
+        }
+        lastUl = lastUl.getParent().getParent();
+        lastLi = makeLi(el);
+        lastUl.adopt(lastLi);
+        lastLevel = lvl;
+      }
+    }
   });
-  return window.nchanSub;
+  
+  if(headingsFound > 0 ){
+    var toggle = new Element("a", {"class": "toggle", href:"#"});
+    toggle.addEvent('click', function(ev) {
+      ev.stop();
+      tocContainer.toggleClass("tableOfContentsHidden");
+    });
+    tocContainer.adopt(toggle);
+    
+    tocContainer.adopt(new Element("h2", {text: "Contents"}));
+    tocContainer.adopt(toc);
+  }
 }
 
-//load!
 addEvent('domready', function() {
-  newSub();
+  insertTableOfContents(".tableOfContents");
 });
-
-/*addEvent('load', function() {
-  window.nchanSub.start();
-});
-*/
