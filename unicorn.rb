6865fbc86ee5cafbaa2e8ab0a9318ff43a80554b
Leo P. <junk@slact.net>
2024-07-30 10:50:02 -0400

    don't listen on port, just socket pls

--- unicorn.rb
+++ unicorn.rb
@@ -20,7 +20,7 @@ worker_processes 20
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
 listen "/tmp/nchapp_unicorn.sock", :backlog => 64
-listen 9293, :tcp_nopush => true
+#listen 9293, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
